02670f94e36fc9950b190c1accbbf6a6bb51cf63
Paula Schuppe <107902047+Paulas12@users.noreply.github.com>
2024-09-16 21:45:43 -0300

    Add files via upload

---
+++ Estructura Elif.py
@@ -0,0 +1,11 @@
+#If - Elif - Else: nos permite especificar múltiples condiciones y bloques de código alternativos
+calificacion = 85
+
+if calificacion >= 90: 
+    print("Excelente")
+elif calificacion >=80: 
+    print("Muy bueno")
+elif calificacion >= 70: 
+    print("Bueno")
+else: 
+    print("Necesita mejorar")
---
+++ hola_mundo y estructura if.py
@@ -0,0 +1,13 @@
+print("¡Hola, Mundo!")
+
+#If - Else
+
+edad = 15
+
+if edad > 18: 
+    #Bloque de código si la condición es verdadera
+    print("Eres mayor de edad")
+else: 
+    #Bloque de código si la condición es falsa
+    print("Eres menor de edad")
+
